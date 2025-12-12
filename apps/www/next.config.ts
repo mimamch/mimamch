e4269bb9d64fdbdbf110ebaa79af4ee0ea32515d
mimamch <mimamch28@gmail.com>
2025-12-12 13:50:47 +0800

    feat(build): add Dockerfile for multi-stage build and deployment

--- apps/www/next.config.ts
+++ apps/www/next.config.ts
@@ -1,7 +1,10 @@
+/* eslint-disable turbo/no-undeclared-env-vars */
 import { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: process.env.BUILD_STANDALONE === "1" ? "standalone" : undefined,
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
